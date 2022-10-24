ecf0a1f8d65d3982ccb2ba44618d90f7f77b577d
JonMaslow <114214959+JonMaslow@users.noreply.github.com>
2022-10-25 02:29:27 +0500

    Task 3 Разница макс и мин значения в случайном массиве

---
+++ Task_3/Program.cs
@@ -0,0 +1,56 @@
+﻿/*Задача 38:
+Задайте массив вещественных чисел.
+Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76
+*/
+
+double [] InitRandomArray (int length)
+{
+    double [] resultArray = new double [length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = rnd.Next(-100,100);
+    }
+    return resultArray;
+}
+
+void PrintArray(double [] array)
+{   Console.WriteLine("Случайный массив:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int GetNumber(string messege)
+{
+    Console.WriteLine(messege);
+    int resultArray = int.Parse(Console.ReadLine()??"");
+    return resultArray;
+}
+
+double GetDiffMaxMin(double [] array)
+{
+    double diffNumber = 0;
+    double maxNumber = array[0];
+    double minNumber = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] >= maxNumber)
+        maxNumber = array[i];
+        if (array[i] <= minNumber)
+        minNumber = array[i];
+    }
+    Console.WriteLine($"Максимально значение в массиве -> {maxNumber}");
+    Console.WriteLine($"Минимально значение в массиве -> {minNumber}");
+    diffNumber = maxNumber - minNumber;
+    return diffNumber;
+}
+int length = GetNumber("Введите длину массива:");
+double [] array = InitRandomArray(length);
+PrintArray(array);
+double diffMaxMin = GetDiffMaxMin(array);
+Console.WriteLine($"Разница между максимальным и минимальным значением в массиве составит {diffMaxMin} единиц");
